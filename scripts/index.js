9de0b4d2df13149901a5558fe8ac2c532b9e9be1
ArthurVBS <arthurvini2703@gmail.com>
2022-02-12 11:37:04 -0300

    Feat: Conversor de temperatura

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const api = {
-    key: "64ed82577ced7f69cb1687f0ce536131",
-    base: "https://api.openweathermap.org/data/2.5/",
-    lang: "pt_br",
-    units: "metric"
+    key: '64ed82577ced7f69cb1687f0ce536131',
+    base: 'https://api.openweathermap.org/data/2.5/',
+    lang: 'pt_br',
+    units: 'metric'
 }
 
 const search = {
@@ -25,14 +25,22 @@ const temp = {
     humidity: document.querySelector('.humidity')
 }
 
-search['button'].addEventListener('click', function() {
-    searchResults(search['input'].value)
+const unit = {
+    unitMetric: document.getElementById('metric'),
+    unitImperial: document.getElementById('imperial')
+}
+
+unit.unitMetric.addEventListener('click', changeFahrenheitToCelsius)
+unit.unitImperial.addEventListener('click', changeCelsiusToFahrenheit)
+
+search.button.addEventListener('click', function() {
+    searchResults(search.input.value)
 })
 
 search.input.addEventListener('keypress', function(event) {
     key = event.keyCode
     if (key === 13) {
-        searchResults(search['input'].value)
+        searchResults(search.input.value)
     }
 })
 
@@ -42,43 +50,83 @@ function searchResults(city) {
             if (!response.ok) {
                 throw new Error(`http error: status ${response.status}`)
             }
-            return response.json();
+            return response.json()
         })
         .catch(error => {
             alert(error.message)
         })
         .then(response => {
             displayResults(response)
-        });
+        })
 }
 
 function displayResults(weather) {
     console.log(weather)
 
-    info.local.innerText = `${weather.name}, ${weather.sys.country}`;
+    info.local.innerText = `${weather.name}, ${weather.sys.country}`
     info.desc.innerText = weather.weather[0].description
 
-    let now = new Date();
-    info.date.innerText = dateBuilder(now);
+    let now = new Date()
+    info.date.innerText = dateBuilder(now)
 
-    let iconName = weather.weather[0].icon;
-    info.img.innerHTML = `<img src="./images/icons/${iconName}.png" alt="Ícone do clima">`
+    let iconName = weather.weather[0].icon
+    info.img.innerHTML = `<img src='./images/icons/${iconName}.png' alt='Ícone do clima'>`
     
     temp.tempNow.innerHTML = Math.round(weather.main.temp)
     temp.tempLow.innerText = Math.round(weather.main.temp_min)
     temp.tempHigh.innerText = Math.round(weather.main.temp_max)
-    temp.tempUnit.innerHTML = `°C`
+    unit.unitMetric.checked ? temp.tempUnit.innerHTML = `°C` : temp.tempUnit.innerHTML = `°F`
     temp.humidity.innerText = `${weather.main.humidity} %`
 }
 
 function dateBuilder(d) {
-    let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julio", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    let days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julio', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
 
-    return `${day}, ${date} ${month} ${year}`;
+    let phrase = `${day}, ${date} ${month} ${year}`
+
+    return phrase
+}
+
+function changeFahrenheitToCelsius()
+{
+    unit.unitMetric.checked ? api.units = 'metric' : api.units = 'imperial'
+    
+    if (temp.tempUnit.outerText == '°F')
+    {
+        temp.tempUnit.innerHTML = '°C'
+        temp.tempNow.innerHTML = calcFahrenheitToCelsius(temp.tempNow.outerText)
+        temp.tempLow.innerHTML = calcFahrenheitToCelsius(temp.tempLow.outerText)
+        temp.tempHigh.innerHTML = calcFahrenheitToCelsius(temp.tempHigh.outerText)
+    }
+}
+
+function changeCelsiusToFahrenheit()
+{
+    unit.unitMetric.checked ? api.units = 'metric' : api.units = 'imperial'
+
+    if (temp.tempUnit.outerText == '°C')
+    {
+        temp.tempUnit.innerHTML = '°F'
+        temp.tempNow.innerHTML = calcCelsiusToFahrenheit(temp.tempNow.outerText)
+        temp.tempLow.innerHTML = calcCelsiusToFahrenheit(temp.tempLow.outerText)
+        temp.tempHigh.innerHTML = calcCelsiusToFahrenheit(temp.tempHigh.outerText)
+    }
+}
+
+function calcFahrenheitToCelsius(temp)
+{
+    newTemp = Math.round((temp - 32) / 1.8)
+    return newTemp
+}
+
+function calcCelsiusToFahrenheit(temp)
+{
+    newTemp = Math.round((temp * 1.8) + 32)
+    return newTemp
 }
